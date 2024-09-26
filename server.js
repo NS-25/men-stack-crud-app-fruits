e42bae617de6e835843c04acfb53b14b2837fa45
George-25 <georgemallory26@gmail.com>
2024-09-26 16:31:08 -0400

    add-show page

--- server.js
+++ server.js
@@ -36,6 +36,15 @@ app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
 
+/// fruits/new
+app.get("/fruits/:fruitId", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/show.ejs", { fruit: foundFruit });
+});
+
+
+
+
 // POST /fruits
 app.post("/fruits", async (req, res) => {
   if (req.body.isReadyToEat === "on") {
@@ -44,9 +53,13 @@ app.post("/fruits", async (req, res) => {
     req.body.isReadyToEat = false;
   }
   await Fruit.create(req.body);
-  res.redirect("/fruits/new");
+  res.redirect("/fruits");
 });
-
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  res.render("fruits/index.ejs", { fruits: allFruits });
+});
 
 
 app.listen(3000, () => {
